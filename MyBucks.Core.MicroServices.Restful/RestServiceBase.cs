429f53772a39256aefac54088450713bf85ab2a9
Tjaart Blignaut <tjaartblig@gmail.com>
2018-06-29 11:04:20 +0200

    initial commit

---
+++ MyBucks.Core.MicroServices.Restful/RestServiceBase.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyBucks.Core.Restful.Filters;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
+
+namespace MyBucks.Core.Restful
+{
+   public abstract class RestServiceBase
+    {
+        private Container container = new Container();
+        
+        public IConfiguration Configuration { get; }
+        
+        public RestServiceBase(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private void LoadAppServices()
+        {
+            LoadStaticsIntoContainer(container);
+            RegistereServices(container);
+            
+        }
+
+        protected abstract void RegistereServices(Container container1);
+        
+
+        protected abstract void LoadStaticsIntoContainer(Container container1);
+
+
+        private void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<IControllerActivator>(
+                new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(container));
+
+
+            services.EnableSimpleInjectorCrossWiring(container);
+            services.UseSimpleInjectorAspNetRequestScoping(container);
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            // Add application services. For instance:
+            LoadAppServices();
+
+            // Cross-wire ASP.NET services (if any). For instance:
+            container.CrossWire<ILoggerFactory>(app);
+
+            // NOTE: Do prevent cross-wired instances as much as possible.
+            // See: https://simpleinjector.org/blog/2016/07/
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.Filters.Add<CheckKongAuthHeader>();
+            });
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+            
+            services.AddApiVersioning(o => o.ApiVersionReader = new UrlSegmentApiVersionReader());
+
+            IntegrateSimpleInjector(services);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            InitializeContainer(app);
+            app.UseMvc();
+        }
+    }
+}
