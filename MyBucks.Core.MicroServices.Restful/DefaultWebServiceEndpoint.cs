cf8d1375cdadfed23dd7488cb197a7c3c589303e
Tjaart Blignaut <tjaartblig@gmail.com>
2018-07-03 11:06:45 +0200

    add default service endpoint

---
+++ MyBucks.Core.MicroServices.Restful/DefaultWebServiceEndpoint.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using MyBucks.Core.MicroServices.Abstractions;
+using MyBucks.Core.MicroServices.ConfigurationModels;
+using Serilog;
+
+namespace MyBucks.Core.MicroServices.Restful
+{
+    public class DefaultWebServiceEndpoint<TStartup> : IServiceEndpoint where TStartup : class
+    {
+       
+            private ILogger _logger;
+            private WebServiceSettings _webServiceSettings = null;
+            private IWebHost _host;
+
+            public DefaultWebServiceEndpoint(ILogger logger, WebServiceSettings webServiceSettings)
+            {
+                _logger = logger;
+                _webServiceSettings = webServiceSettings;
+            }
+
+            public void StartServer()
+            {
+
+                var url = $"http://*:{_webServiceSettings.PortNumber}";
+                _host = new WebHostBuilder()
+                    .UseUrls(url)
+                    .UseKestrel()
+                    .UseSerilog(_logger)
+                    .UseStartup<TStartup>()
+                    .Build();
+                _host.Start();
+                _logger.Information("Hosting on {host}", url);
+            }
+
+            public void StopServer()
+            {
+                _logger.Information("Stopping API host");
+                _host.StopAsync().Wait();
+            }
+
+            public virtual string EndpointDescription => "Restful API Service";
+        
+    }
+}
