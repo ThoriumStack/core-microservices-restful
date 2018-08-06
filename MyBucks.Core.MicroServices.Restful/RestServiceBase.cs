c5213b846c5bac975060e73ed26f24f4be8f3989
Ruan Strydom <ruans@mybucks.com>
2018-08-06 11:58:23 +0200

    New Base and Basic Rest Service

--- MyBucks.Core.MicroServices.Restful/RestServiceBase.cs
+++ MyBucks.Core.MicroServices.Restful/RestServiceBase.cs
@@ -1,105 +1,28 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Versioning;
-using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using MyBucks.Core.MicroServices.Abstractions;
-using MyBucks.Core.MicroServices.Restful.Filters;
-using SimpleInjector;
-using SimpleInjector.Integration.AspNetCore.Mvc;
-using SimpleInjector.Lifestyles;
 
 namespace MyBucks.Core.MicroServices.Restful
 {
-   public abstract class RestServiceBase
+   public abstract class BasicRestService : RestServiceBase
     {
-        private readonly IServiceStartup _startup;
-        private Container container = new Container();
+        protected BasicRestService(IConfiguration configuration, IServiceStartup startup) :
+            base(configuration, startup)
+        { }
         
-        public IConfiguration Configuration { get; }
-        
-        public RestServiceBase(IConfiguration configuration, IServiceStartup startup)
+        protected new virtual void ConfigureCustomServices(IServiceCollection services)
         {
-            _startup = startup;
-            Configuration = configuration;
-        }
-
-        private void LoadAppServices()
-        {
-            ServiceStartup.ContainerSetup(container, _startup);
-        }
-
-
-        private void IntegrateSimpleInjector(IServiceCollection services)
-        {
-            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            services.AddSingleton<IControllerActivator>(
-                new SimpleInjectorControllerActivator(container));
-            services.AddSingleton<IViewComponentActivator>(
-                new SimpleInjectorViewComponentActivator(container));
-
-
-            services.EnableSimpleInjectorCrossWiring(container);
-            services.UseSimpleInjectorAspNetRequestScoping(container);
-        }
-
-        private void InitializeContainer(IApplicationBuilder app)
-        {
-            // Add application presentation components:
-            container.RegisterMvcControllers(app);
-            container.RegisterMvcViewComponents(app);
-
-            // Add application services. For instance:
-            LoadAppServices();
-
-            // Cross-wire ASP.NET services (if any). For instance:
-            container.CrossWire<ILoggerFactory>(app);
-            container.Verify();
-            // NOTE: Do prevent cross-wired instances as much as possible.
-            // See: https://simpleinjector.org/blog/2016/07/
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc(options =>
-            {
-                options.Filters.Add<CheckKongAuthHeader>();
-            });
-
-            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            {
-                builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader();
-            }));
-            
+            services.AddMvc();
             services.AddApiVersioning(o => o.ApiVersionReader = new UrlSegmentApiVersionReader());
-            ConfigureCustomServices(services);
-            IntegrateSimpleInjector(services);
         }
-
-        protected virtual void ConfigureCustomServices(IServiceCollection services)
+        
+        protected new virtual void ConfigureApp(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            InitializeContainer(app);
             app.UseMvc();
-            ConfigureApp(app, env);
-        }
-
-        protected virtual void ConfigureApp(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            
+            base.ConfigureApp(app, env);
         }
     }
 }
---
+++ MyBucks.Core.MicroServices.Restful/RestStartupBase.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyBucks.Core.MicroServices.Abstractions;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
+
+namespace MyBucks.Core.MicroServices.Restful
+{
+    public abstract class RestServiceBase
+    {
+        private readonly IServiceStartup _startup;
+        private readonly IConfiguration _configuration;
+        private readonly Container _container = new Container();
+        
+        protected RestServiceBase(IConfiguration configuration, IServiceStartup startup)
+        {
+            _configuration = configuration;
+            _startup = startup;
+        }
+
+        protected IConfiguration Configuration => _configuration;
+        
+        protected void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureCustomServices(services);
+            IntegrateSimpleInjector(services);
+        }
+        
+        protected void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            InitializeContainer(app);
+            ConfigureApp(app, env);
+        }
+        
+        protected virtual void ConfigureCustomServices(IServiceCollection services)
+        {
+        }
+        
+        protected virtual void ConfigureApp(IApplicationBuilder app, IHostingEnvironment env)
+        {
+        }
+        
+        private void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            _container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<IControllerActivator>(
+                new SimpleInjectorControllerActivator(_container));
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(_container));
+
+            services.EnableSimpleInjectorCrossWiring(_container);
+            services.UseSimpleInjectorAspNetRequestScoping(_container);
+        }
+        
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            // Add application presentation components:
+            _container.RegisterMvcControllers(app);
+            _container.RegisterMvcViewComponents(app);
+
+            // Add application services. For instance:
+            LoadAppServices();
+
+            // Cross-wire ASP.NET services (if any). For instance:
+            _container.CrossWire<ILoggerFactory>(app);
+            _container.Verify();
+            // NOTE: Do prevent cross-wired instances as much as possible.
+            // See: https://simpleinjector.org/blog/2016/07/
+        }
+        
+        private void LoadAppServices()
+        {
+            ServiceStartup.ContainerSetup(_container, _startup);
+        }
+    }
+}
